3002c95daece7aaac090b191877438b5b2178f2e
Deden Rustiana <rustianadeden25@gmail.com>
2022-08-10 14:45:49 +0700

    tester

---
+++ login.sh
@@ -0,0 +1,32 @@
+--login
+mongo -u denrus -p denrus
+
+--check status connection
+db.runCommand("connectionStatus")
+
+show dbs
+
+use Test
+
+show collections
+
+--test insert
+db.movie.insert({"name":"tutorials point"})
+
+db.items.insert({"item_name":"Laptop"})
+
+db.items.insert({"employee": {"name": "sonoo","salary": 56000,"married": true}})
+
+db.items.insert({
+    "employee": {"name": "sonoo","salary": 56000,"married": true, "join_date": "2022-01-31",
+        "departement":{
+            "dept_id": 1,
+            "dept_name":"mis"
+        }},
+})
+
+
+
+db.items.find()
+
+db.items.find("62f356a066ad7ecab6356d35")
